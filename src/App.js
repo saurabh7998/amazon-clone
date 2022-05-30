2880ce1e405200d36a4fb2f519f12c5b88e4b34f
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-05-30 19:39:34 -0400

    sets up the routes using react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path='/login' element={<h1>Login</h1>}/>
+          <Route path='/checkout' element={<h1>Checkout</h1>}/>
+          <Route path='/' element={<h1>Home</h1>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
