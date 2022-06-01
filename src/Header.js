ab6d47a27f98189ac6933c5c7ed6a41c6d11e4e2
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-05-31 20:29:20 -0400

    navbar design completed

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 import './Header.css';
 
 const Header = () => {
     return(
         <nav className="header">
             {/* amazon logo on the left */}
-            <img 
-                className="header__logo"
-                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="amazon logo"
-            />
+            <Link to={'/'}>
+                <img 
+                    className="header__logo"
+                    src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="amazon logo"
+                />
+            </Link>
+            
+            {/* search bar with icon */}
+            <div className="header__search">
+                <input type={"text"} className="header__searchInput"/>
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+
+            {/* Three links */}
+            <div className="header__navLinks">
+                {/* 1st link */}
+                <Link to={'/login'} className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Saurabh</span>
+                        <span className="header__optionLineTwo"> Sign in</span>
+                    </div>
+                </Link>
+
+                {/* 2nd Link */}
+                <Link to={'/login'} className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo"> & Orders </span>
+                    </div>
+                </Link>
+
+                {/* 3rd Link */}
+                <Link to={'/login'} className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo"> Prime</span>
+                    </div>
+                </Link>
+
+                {/* 4th Link */}
+                <Link to={'/login'} className="header__link">
+                    <div className="header__shoppingCart">
+                        {/* Shopping cart icon */}
+                        <ShoppingCartIcon />
+                        {/* No. of items in the cart */}
+                        <span className="header__optionCartCount">0</span>
+                    </div>
+                </Link>
+
+            </div>
+            
+
         </nav>
+
     )
 }
 
