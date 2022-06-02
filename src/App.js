2730288c0c07d796a4afbf3164de4b919bfc9524
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-06-01 22:10:54 -0400

    adds Home page banner image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Header from './Header';
+import { Fragment } from 'react';
+import Home from './Home';
 
 function App() {
   return (
@@ -8,8 +10,18 @@ function App() {
       <div className="app">
         <Routes>
           <Route path='/login' element={<h1>Login</h1>}/>
-          <Route path='/checkout' element={<h1>Checkout</h1>}/>
-          <Route path='/' element={<Header/>}/>
+          <Route path='/checkout' element={
+              <Fragment>
+                  <Header/>
+                  <h1>Checkout</h1>
+              </Fragment>   
+          }/>
+          <Route path='/' element={
+              <Fragment>
+                  <Header/>
+                  <Home/>
+              </Fragment>
+            }/>
         </Routes>
       </div>
     </Router>
